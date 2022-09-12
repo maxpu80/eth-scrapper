804be88ca5c62d6fb38e2bd3cc15fde7a09def15
maxpu80 <max.putilov@gmail.com>
2022-09-12 20:13:03 +0300

    docker-compose up all services

--- node/scrapper-actor/index.ts
+++ node/scrapper-actor/index.ts
@@ -2,17 +2,15 @@ import { DaprServer } from "@dapr/dapr";
 
 import ScrapperActor from "./scrapper-actor";
 
-const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
-const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3502";
-const SERVER_HOST = process.env.SERVER_HOST || "127.0.0.1";
-const SERVER_PORT = process.env.SERVER_PORT || "5002";
+const SERVER_PORT = process.env.PORT || process.env.SERVER_PORT || "5002";
 
 async function main() {
-  const server = new DaprServer(SERVER_HOST, SERVER_PORT, DAPR_HOST, DAPR_HTTP_PORT);
+  const server = new DaprServer(undefined, SERVER_PORT);
 
   await server.actor.init(); // Let the server know we need actors
   await server.actor.registerActor(ScrapperActor); // Register the actor
   await server.start();
+  console.log(`scrapper-actor::started on port ${SERVER_PORT}`)
 }
 
 main().catch((e) => console.error(e));
--- node/scrapper-actor/readme.md
+++ node/scrapper-actor/readme.md
@@ -2,5 +2,18 @@
 
 Read events given address, abi, and block from (opt) to (opt)
 
-`yarn start:dapr`
+```
+$Env:PORT=5002
 
+dapr run --app-port $Env:Port --app-id scrapper-actor --components-path ../../components --dapr-http-port 3502 -- npm run start
+```
+
+with build 
+
+```
+cd build 
+
+$Env:PORT=5002
+
+dapr run --app-port $Env:Port --app-id scrapper-actor --components-path ../../../components --dapr-http-port 3502 -- node index.js
+```
